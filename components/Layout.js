a3b8aeda15451cd171490b03675583ed3c6bcee4
Nikita <nikitapashinsky@gmail.com>
2023-03-06 12:10:09 +0300

    add tailwind

---
+++ components/Layout.js
@@ -0,0 +1,67 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+const name = "Nikita Pashinsky";
+export const siteTitle = "Next.js Sample Website";
+
+export default function Layout({ children, home }) {
+  return (
+    <div className="flex flex-col items-center w-full max-w-sm mx-auto mt-12 gap-2">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className="flex flex-col gap-6 items-center">
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/pfp.jpg"
+              className="rounded-full"
+              height={144}
+              width={144}
+              alt=""
+            />
+            <h1 className="text-xl font-semibold">{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <Image
+                priority
+                src="/images/pfp.jpg"
+                className=""
+                height={108}
+                width={108}
+                alt=""
+              />
+            </Link>
+            <h2 className="">
+              <Link href="/" className="">
+                {name}
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className="">
+          <Link href="/">← Back to home</Link>
+        </div>
+      )}
+    </div>
+  );
+}
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,15 @@
 import Link from "next/link";
 import Head from "next/head";
+import Layout from "../../components/Layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First post</title>
       </Head>
       <h1>First post</h1>
       <Link href="/">Back home</Link>
-    </>
+    </Layout>
   );
 }
